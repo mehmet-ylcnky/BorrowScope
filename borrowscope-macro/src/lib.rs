9379a0d81553b4ab0d2929b781058e92e2c7c534
mehmet-yalcinkaya_volvo <mehmet.yalcinkaya@volvo.com>
2025-10-31 11:50:57 +0100

    Chapter 2 Section 10: Set up procedural macro infrastructure

--- borrowscope-macro/src/lib.rs
+++ borrowscope-macro/src/lib.rs
@@ -4,6 +4,9 @@
 //! Rust code to track ownership and borrowing operations at runtime.
 
 use proc_macro::TokenStream;
+use proc_macro_error::{abort, proc_macro_error};
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
 /// Attribute macro to trace ownership and borrowing in a function
 ///
@@ -16,7 +19,21 @@ use proc_macro::TokenStream;
 /// }
 /// ```
 #[proc_macro_attribute]
+#[proc_macro_error]
 pub fn trace_borrow(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    // Placeholder implementation - will be expanded in Chapter 2
-    item
+    // Parse the input as a function
+    let input_fn = parse_macro_input!(item as ItemFn);
+
+    // Validate it's a function
+    if input_fn.sig.ident.to_string().is_empty() {
+        abort!(input_fn.sig.ident, "trace_borrow can only be applied to functions");
+    }
+
+    // For now, just return the function unchanged
+    // We'll add instrumentation in later sections
+    let output = quote! {
+        #input_fn
+    };
+
+    output.into()
 }
